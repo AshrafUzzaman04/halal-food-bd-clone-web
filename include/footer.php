d5667cf059e0be2b264af5e17f3eca22b1e32e26
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-03-23 12:24:58 +0600

    product page added

--- include/footer.php
+++ include/footer.php
@@ -86,8 +86,8 @@
 <!-- Initialize Swiper -->
 <script>
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 4,
+        spaceBetween: 40,
         freeMode: true,
         navigation: {
             nextEl: ".swiper-button-next",
@@ -97,6 +97,29 @@
             el: ".swiper-pagination",
             clickable: true,
         },
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 0px
+            0: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 40
+            }
+        }
     });
 </script>
 
--- product.php
+++ product.php
@@ -8,27 +8,66 @@ include_once("./include/navbar.php");
 <section class="pro_detls">
 
 </section>
-
 <!-- =================================== -->
 <!-- ====== related products section ====== -->
 <!-- =================================== -->
 <section class="related_pro">
-    <!-- Swiper -->
-    <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
-            <div class="swiper-slide"><img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" /></div>
+    <div class="container-lg">
+        <!-- Swiper -->
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <?php
+                for ($i = 0; $i < 10; $i++) {
+                ?>
+                    <div class="swiper-slide">
+                        <!-- product 1 -->
+                        <div class="pro">
+                            <div class="img_pro">
+                                <a href="#">
+                                    <img class="img-fluid" src="https://halalfoodservicebd.com/wp-content/uploads/2021/12/299106266_601731048168033_8846127039959966928_n.jpg" alt="" />
+                                </a>
+                                <div class="wish_list">
+                                    <a href="#"><i class="fa-solid fa-heart"></i></a>
+                                </div>
+                                <div class="quick_view" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    <!-- Button trigger Quick View modal -->
+                                    <a href="javascript:void(0)"><strong>Quick View</strong></a>
+                                </div>
+                            </div>
+                            <div class="article_section text-start">
+                                <article>
+                                    <span class="d-block">Foods</span>
+                                    <span class="d-block"><a href="#" class="d-inline-block">দুলালের তালমিছরি (১ কেজি)</a></span>
+                                    <span class="text-dark">From: <strong class="fw-bold">৳ 490.00</strong></span>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                        Modal title
+                    </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">hi</div>
+            </div>
+        </div>
     </div>
 </section>
+
 <?php
 include_once("./include/footer.php");
 ?>
